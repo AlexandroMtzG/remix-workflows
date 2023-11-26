a8125111d40d750634f75a20d1c3523e5489c48a
AlexandroMtzG <alex.martinez@absys.com.mx>
2023-11-26 14:25:54 -0600

    remove server config

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,6 @@ module.exports = {
   // When running locally in development mode, we use the built-in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   serverBuildPath: "api/index.js",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
